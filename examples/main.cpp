56212433dd71637e05822e0289af25f1ca80b1bb
Grady O'Connell <flipcoder@gmail.com>
2012-02-17 14:02:12 -0800

    tabs to spaces (via script)

--- examples/main.cpp
+++ examples/main.cpp
@@ -11,41 +11,41 @@ using namespace std;
 
 string getTime()
 {
-	time_t now;
-	time ( &now );
-	stringstream stream;
-	stream << time( &now ) << flush;
-	return stream.str();
+    time_t now;
+    time ( &now );
+    stringstream stream;
+    stream << time( &now ) << flush;
+    return stream.str();
 }
 
 int main(int argc, char** argv)
 {
-	auto_ptr<SpadeScriptEngine> engine(new SpadeScriptEngine());
-	auto_ptr<SpadeScript> object(new SpadeScript());
-	auto_ptr<SpadeScriptDataSet> dataset(new SpadeScriptDataSet());
-	auto_ptr<SpadeScriptContext> defaultContext(new SpadeScriptContext());
+    auto_ptr<SpadeScriptEngine> engine(new SpadeScriptEngine());
+    auto_ptr<SpadeScript> object(new SpadeScript());
+    auto_ptr<SpadeScriptDataSet> dataset(new SpadeScriptDataSet());
+    auto_ptr<SpadeScriptContext> defaultContext(new SpadeScriptContext());
 
-	dataset->load("scripts/dataset.lib.ssf", engine.get());
+    dataset->load("scripts/dataset.lib.ssf", engine.get());
 
-	string script = "";
+    string script = "";
 
-	cout<<"Spade Script"<<endl;
-	cout<<"Filename> ";
-	getline(cin, script);
+    cout<<"Spade Script"<<endl;
+    cout<<"Filename> ";
+    getline(cin, script);
 
-	if(object->load(script, engine.get()))
-		cout<<"Started at: "<<getTime()<<endl;
-	else
-		return 0;
+    if(object->load(script, engine.get()))
+        cout<<"Started at: "<<getTime()<<endl;
+    else
+        return 0;
 
-	defaultContext->start();
-	string r = "";
-	if(object->call("main", defaultContext.get(), r, "default")) {
-		cout << "Script completed successfully. at " <<getTime()<< endl;
-	}
-	defaultContext->stop();
+    defaultContext->start();
+    string r = "";
+    if(object->call("main", defaultContext.get(), r, "default")) {
+        cout << "Script completed successfully. at " <<getTime()<< endl;
+    }
+    defaultContext->stop();
 
-	system("pause");
+    system("pause");
 
-	return 0;
+    return 0;
 }
